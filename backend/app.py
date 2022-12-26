60fc26d8b2b48417eea418876e7eb8f435e3ca77
Ichinga-Samuel <ichingasamuel@gmail.com>
2022-12-26 14:42:46 +0100

    ready

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,7 @@
+from logging import getLogger
+
 from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException
 from fastapi.responses import JSONResponse
@@ -6,20 +9,20 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
 from tortoise.contrib.fastapi import register_tortoise
 
-# from app.utils.env import env
 from routes.auth import router as auth_router
 from routes.report import router as report_router
 from utils.db import TORTOISE_ORM
 from utils import ResponseModel
 
+logger = getLogger()
 app = FastAPI()
 
 register_tortoise(app, config=TORTOISE_ORM, generate_schemas=True, add_exception_handlers=True)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex="http://localhost:[0-9]{4,5}",
-    # allow_credentials=True,
+    allow_origins=['http://localhost:4200', "https://moniewatch.web.app", "https://localhost:4200"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -33,10 +36,14 @@ async def http_exception_handler(request: Request, exc: HTTPException):
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    print(exc.errors())
+    logger.error(exc.errors())
     res = ResponseModel(message="Bad input data", data=exc.body, status=False)
     return JSONResponse(status_code=200, content=jsonable_encoder(res))
 
-
 app.include_router(auth_router)
 app.include_router(report_router)
+
+
+@app.get('/')
+async def home():
+    return RedirectResponse("/docs")
